f0ef78d361c46357c37184f84482acbc7da7cebd
israa maher <you@example.com>
2023-12-18 15:48:17 -0800

    selection sort 1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,15 +11,16 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for ( j = i + 1;j < size; j++)
-			if (array[j] < array[min]) min = j;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min])
+				min = j;
 
 		if (min !=  i)
 		{
 			tmp = array[i];
 			array[i] = array[min];
 			array[min] = tmp;
-			print_array(array ,size);
+			print_array(array, size);
 		}
 	}
 }
